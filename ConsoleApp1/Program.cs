f1d9b8b086d86d617f061208387048fc5aa63e5c
Tosking <tosking@duck.com>
2023-03-03 02:52:23 +0500

    added image generator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,13 @@ using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using System.IO;
-using System.Drawing;
+using System.Net.Mime;
+using System.Numerics;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp.Drawing.Processing;
 using Color = Telegram.Bot.Types.Color;
 using File = Telegram.Bot.Types.File;
 
@@ -31,24 +37,72 @@ var me = await botClient.GetMeAsync();
 System.Console.ReadLine();
 cts.Cancel();
 
-Stream demotivate(string text, string photoName)
+void demotivate_Text(string text, string save)
 {
-    Image photo = 
-    using Stream stream = System.IO.File.OpenRead("./photo.jpg");
-    return stream;
+    Image photo = Image.Load(save);
+    var size = 50;
+    Font font = SystemFonts.CreateFont("Arial", size, FontStyle.Regular);
+    photo.Mutate(x => x.DrawText(text, font, SixLabors.ImageSharp.Color.White, new PointF((float) photo.Width / 2 - (size / 4 * text.Length) - size / 2, photo.Height / 2 + 100 )));
+    photo.Save(save);
+    //using Stream stream = System.IO.File.OpenRead("./photo.jpg");
+    //return stream;
+    
+}
+
+void demotivate_Photo(string photoName, string save)
+{
+    Image<Rgba64> photo = new(640, 480); 
+    Image image = Image.Load(photoName);
+    image.Mutate(x => x.Resize(300, 200));
+    photo.Mutate(x => x.DrawImage(image, new Point(photo.Width / 2 - 150, photo.Height/ 2 - 150), 1));
+    photo.Save(save);
 }
 
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
     if (update.Message is not { } message)
         return;
-    if (message.Text is not { } messageText)
-        return;
+    var image = "./" + message.Chat.Id + "i.jpg";
+    var photo = "./" + message.Chat.Id + "p.jpg"; 
+    if (update.Message.Type == MessageType.Photo)
+    {
+        string destinationFilePath = image;
 
-    Message sendMessage = await botClient.SendPhotoAsync(
-        chatId: message.Chat.Id,
-        photo: new InputFile(content:stream, fileName: "photo.jpg"),
-        cancellationToken: cancellationToken);
+        await using Stream fileStream = System.IO.File.OpenWrite(destinationFilePath); 
+        var fileId = update.Message.Photo.Last().FileId;
+        var fileInfo = await botClient.GetFileAsync(fileId);
+        var filePath = fileInfo.FilePath;
+        await botClient.DownloadFileAsync(
+            filePath: filePath,
+            destination: fileStream,
+            cancellationToken: cancellationToken);
+        fileStream.Close();
+        demotivate_Photo(destinationFilePath, photo);
+    }
+
+    if (update.Message.Type == MessageType.Text)
+    {
+        if (System.IO.File.Exists(photo) == false)
+        {
+            await botClient.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: "Send photo first",
+                cancellationToken: cancellationToken
+            );
+            return;
+        }
+
+        System.Console.WriteLine("123321");
+        demotivate_Text(update.Message.Text, photo);
+        var mes = System.IO.File.OpenRead(photo);
+        Message sendMessage = await botClient.SendPhotoAsync(
+            chatId: message.Chat.Id,
+            photo: new InputFile(content:mes, fileName: photo),
+            cancellationToken: cancellationToken);
+        mes.Close();
+        System.IO.File.Delete(photo);
+    }
+
 }
 
 Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
